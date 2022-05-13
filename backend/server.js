f646830e9f704451ec099a4dee03e2e2ef87bdec
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-13 17:01:17 -0500

    Server initialized

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,11 @@
-console.log('Hello World')
+const express = require('express');
+const dotenv = require('dotenv').config();
 
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({ message: 'Get goals' })
+});
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
