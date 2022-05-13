cee5153a5ef5c02f09ef21f27dc14d9c4bb7b128
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-13 18:27:55 -0500

    Routes Created

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get('/api/goals', (req, res) => {
-    res.status(200).json({ message: 'Get goals' })
-});
+app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
