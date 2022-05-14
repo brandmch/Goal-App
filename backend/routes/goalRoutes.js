649f91ebfc2ef34d6ed6149faed18d50ab14deeb
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-13 19:25:05 -0500

    middleware init

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController');
 
-router.get('/', getGoals);
 
-router.post('/', setGoals);
-
-router.put('/:id', updateGoals);
-
-router.delete('/:id', deleteGoals);
+router.route('/').get(getGoals).post(setGoals);
+router.route('/:id').put(updateGoals).delete(deleteGoals);
 
 module.exports = router; 
