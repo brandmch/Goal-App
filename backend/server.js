2df2be0fc82131a9789a000d6649baa0cabb6028
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-13 19:48:19 -0500

    MongoDB - connected

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 
